a62389b85d74e49618ef4e3737aa04604377756b
JoseRoncal2002 <u201912909@upc.edu.pe>
2021-11-16 23:25:56 -0500

    firebase-auth

---
+++ log-in.js
@@ -0,0 +1,37 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqAL1391tXkK8dTBlR0CSRgaAg9Cv8A7k",
+  authDomain: "impera-finanzas.firebaseapp.com",
+  projectId: "impera-finanzas",
+  storageBucket: "impera-finanzas.appspot.com",
+  messagingSenderId: "306365676217",
+  appId: "1:306365676217:web:872b391e9c31c524ff3968"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const email = document.querySelector('#login__username').value;
+    const password = document.querySelector('#login__password').value;
+
+     // Authenticate the User
+     signInWithEmailAndPassword(auth, email, password)
+     .then((userCredential) => {
+         console.log('login')
+         window.location.replace("rxh.html");
+         // clear the form
+         signUpForm.reset();
+     });
+ 
+})
---
+++ registro.js
@@ -0,0 +1,42 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqAL1391tXkK8dTBlR0CSRgaAg9Cv8A7k",
+  authDomain: "impera-finanzas.firebaseapp.com",
+  projectId: "impera-finanzas",
+  storageBucket: "impera-finanzas.appspot.com",
+  messagingSenderId: "306365676217",
+  appId: "1:306365676217:web:872b391e9c31c524ff3968"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const signupForm = document.querySelector('#signup-form');
+
+signupForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const username = document.querySelector('#login__name').value;
+    const lastname = document.querySelector('#login__lastname').value;
+    const dni = document.querySelector('#login__dni').value;
+    const email = document.querySelector('#login__email').value;
+    const password = document.querySelector('#login__password').value;
+
+     // Authenticate the User
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        console.log('sign')
+        window.location.replace("index.html");
+        // clear the form
+        signUpForm.reset();
+    });
+
+
+})
